d7eeb234614835c68908184eb28d7b509daa622b
Ben DePew <ben@agileworks.io>
2019-05-21 11:46:03 -0400

    HomePage UI Layout

--- Hoop Manager/Hoop Manager/ViewController.swift
+++ Hoop Manager/Hoop Manager/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var BGImg: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
